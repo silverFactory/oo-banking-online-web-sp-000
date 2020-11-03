04d2ad15fea16a426ea79f4c0c7209aa4443afb1
Will Studabaker <will.studabaker@gmail.com>
2020-11-03 16:11:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -4,7 +4,7 @@ class Transfer
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
-    @status = pending
+    @status = "pending"
     @amount = amount
   end
 end
